d8f872e8e2408d1f1015da83b228e6b699c93ac8
Richardaeld <Richardaeld@gmail.com>
2022-01-06 21:40:58 -0500

    Fixed project switching animation

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ let headerBackgroundFilter = document.getElementById('headerBackgroundFilter');
 let toTop = document.getElementById("toTop");
 
 viewHeight = window.innerHeight;
-console.log(viewHeight)
+// console.log(viewHeight)
 
 let view100 = viewHeight;
 let view75 = viewHeight * .75;
@@ -41,6 +41,15 @@ window.addEventListener('scroll', function() {
 })
 
 
+let findProjects = document.querySelectorAll(".projectRow");
+findProjects.forEach(setProjectHeight);
+function setProjectHeight(projectBody) {
+    let itemHeight = projectBody.getElementsByClassName("project-card-text")[0].offsetHeight;
+    projectBody.getElementsByClassName("project-card-text")[0].style.height = itemHeight + "px";
+    console.log(itemHeight)
+}
+
+
 // Adds function to project buttons => hides and reveals projects on click with animation
 let projectButtons = document.querySelectorAll(".project-button-depressed");
 projectButtons.forEach(ProjectButtonFunction);
@@ -52,22 +61,39 @@ function ProjectButtonFunction(item) {
             // find Id of item element
             let idName = item.textContent.replace(/ /g, "-").toLowerCase();
 
-            // Find new element height
-            // let itemHeight = item.offsetHeight;
-            // console.log(itemHeight)
-
             // find previous element
             let findProject = document.querySelectorAll(".projectRow")
             findProject.forEach(hideProject);
             function hideProject(visibleProject) {
-                if (!visibleProject.classList.contains("invis")) {
+                if (!visibleProject.classList.contains("invis-test")) {
                     previousElement = visibleProject
-                    console.log(previousElement)
+                    previousElementHeight = visibleProject.getElementsByClassName("project-card-text")[0].offsetHeight;
+                    // console.log(previousElement, previousElement.offsetHeight, "previous element")
                 }
             }
 
+            // Find height of background element
+            let backgroundHeight = document.getElementsByClassName("backgroundElement")[0].offsetHeight + document.getElementsByClassName("backgroundElement")[1].offsetHeight + document.getElementsByClassName("backgroundElement")[2].offsetHeight + document.getElementsByClassName("backgroundElement")[3].offsetHeight;
+
+
+
+            console.log(backgroundHeight, "background img height - project")
+
+            // Find new element height
+            let itemHeight = document.getElementById(idName).getElementsByClassName("project-card-text")[0].offsetHeight;
+            // console.log(itemHeight, "new height")
+            // console.log(previousElement, previousElement.offsetHeight,"old height")
+            // console.log(document.getElementById(idName),document.getElementById(idName).offsetHeight ,"new height")
+
+            // sets background height
+            document.getElementsByClassName("footer-background")[0].style.height = (backgroundHeight + itemHeight) + "px"
+
             // Animate height change
             // previousElement.getElementsByClassName("text-animate")[0].style.height = itemHeight + "px";
+            // previousElement.parentElement.style.height = (itemHeight) + "px";
+            previousElement.getElementsByClassName("project-card-text")[0].style.height = (itemHeight) + "px";
+            // console.log(previousElement, " setting height here")
+
 
             // Removes button pushed effect
             let removeDepressed = document.querySelector(".project-button-opacity");
@@ -78,16 +104,21 @@ function ProjectButtonFunction(item) {
 
             setTimeout(function() {
                 // Hides previous project
-                previousElement.classList.add("invis")
+                previousElement.classList.add("invis-test")
                 // Reveals selected project
-                document.getElementById(idName).classList.remove("invis")
-            },1000)
+                document.getElementById(idName).classList.remove("invis-test")
             // Image animation
+            document.getElementById(idName).getElementsByClassName("text-animate")[0].classList.add("text-change-anim-finish")
+            // Text animation
             document.getElementById(idName).getElementsByClassName("image-animate")[0].classList.add("img-change-anim-finish")
+            },1000)
+
+            // Image animation
             previousElement.getElementsByClassName("image-animate")[0].classList.add("img-change-anim")
             // Text animation
-            document.getElementById(idName).getElementsByClassName("text-animate")[0].classList.add("text-change-anim-finish")
             previousElement.getElementsByClassName("text-animate")[0].classList.add("text-change-anim")
+
+
 
 
             // Remove animations
@@ -96,6 +127,10 @@ function ProjectButtonFunction(item) {
                 previousElement.getElementsByClassName("image-animate")[0].classList.remove("img-change-anim")
                 document.getElementById(idName).getElementsByClassName("text-animate")[0].classList.remove("text-change-anim-finish")
                 previousElement.getElementsByClassName("text-animate")[0].classList.remove("text-change-anim")
+
+                // resets transitional height of previous project
+                previousElement.getElementsByClassName("project-card-text")[0].style.height = previousElementHeight + "px";
+
             },2000)
 
         }
