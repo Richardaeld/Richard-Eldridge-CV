6da6e0917c652fef1d5957313d7461481022de06
Richardaeld <Richardaeld@gmail.com>
2022-01-11 12:17:54 -0500

    Changed projects animations to feel more coordinated and reduced system tax from JS scroll checks

--- static/js/finished.js
+++ static/js/finished.js
@@ -12,24 +12,28 @@ let view50 = viewHeight * .5;
 let view25 = viewHeight * .25;
 
 
+console.log(typeof(new Date().getTime()))
+
+let previousScrollTime = new Date().getTime()
 
 window.addEventListener('scroll', function() {
+    if (previousScrollTime + 100 < parseInt(new Date().getTime())) {
     if(this.scrollY < viewHeight) {
         // adds toTop button to visibility
         if (! toTop.classList.contains("invis")) {
             toTop.classList.add("invis")
         }
 
-        if(this.scrollY == 0) {
+        // if(this.scrollY == 0) {
+        if (this.scrollY > 0 && this.scrollY < view25) {
             headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, 0)'
-        } else if (this.scrollY > 0 && this.scrollY < view25) {
-            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .45)'
         } else if (this.scrollY > view25 && this.scrollY < view50) {
-            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .75)'
+            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .45)'
         } else if (this.scrollY > view50 && this.scrollY < view75) {
-            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .85)'
+            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .75)'
         } else if (this.scrollY > view75 && this.scrollY < view100) {
-            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .95)'
+            headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .85)'
+            // headerBackgroundFilter.style.backgroundColor = 'rgba(18, 19, 21, .95)'
         }
     } else {
         // removes toTop button from visibility
@@ -37,5 +41,7 @@ window.addEventListener('scroll', function() {
             toTop.classList.remove("invis")
         }
     }
-
+    console.log("I am ehre")
+    previousScrollTime = new Date().getTime()
+}
 })
